1fe0dbe934750c80960a6d2d7f5aae536fa8d4eb
casca <8927157+casca@users.noreply.github.com>
2020-07-27 22:02:03 +0200

    perf(cms): remove netlify widget code from index page

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,11 @@ import Head from 'next/head';
 import { useEffect } from 'react';
 
 export default function Home() {
-  useEffect(() => {
-    if (window.netlifyIdentity) {
-      window.netlifyIdentity.on('init', (user) => {
-        if (!user) {
-          window.netlifyIdentity.on('login', () => {
-            document.location.href = '/admin/';
-          });
-        }
-      });
-    }
-  }, []);
   return (
     <div className="container">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
-        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       </Head>
 
       <main>
